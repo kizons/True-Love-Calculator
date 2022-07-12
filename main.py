b379bc390cb6a7be29fc6f1c2c69d11f7a43bd2e
kizons <38180110+kizons@users.noreply.github.com>
2022-07-12 03:03:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print("WELCOME TO TRUE LOVE CALCULATOR!")
+name = input("What is name ? \n").lower()
+partner = input("What is your partner's name ? \n").lower()
+add_names = str(name) + str(partner)
+
+true = str(add_names.count("t") + add_names.count("r") + add_names.count("u") + add_names.count("e"))
+love = str(add_names.count("l") + add_names.count("o") + add_names.count("v") + add_names.count("e"))
+
+love_score = int(true + love)
+
+if love_score > 50:
+    print(f"Your Love Score is: {love_score}! You are compatible")
+else:
+    print(f"Your Love Score is: {love_score}!\nOMG! You are not compatible")
